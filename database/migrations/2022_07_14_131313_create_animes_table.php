3a3999280d9b38cfb198bf8e685c045297c23cad
Timama <76005360+Timamazzz@users.noreply.github.com>
2022-07-17 12:59:52 +0300

    Modify anime table migration

--- database/migrations/2022_07_14_131313_create_animes_table.php
+++ database/migrations/2022_07_14_131313_create_animes_table.php
@@ -15,14 +15,14 @@ return new class extends Migration
     {
         Schema::create('animes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->default('anime name');
             $table->string('eng_name')->nullable();
             $table->string('jap_name')->nullable();
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->string('image')->default('img/anime-picture.jpg')->nullable();
             $table->unsignedInteger('likes')->default(0);
             $table->unsignedInteger('dislikes')->default(0);
-            $table->unsignedInteger('year');
+            $table->unsignedInteger('year')->nullable();
             $table->timestamps();
         });
     }
