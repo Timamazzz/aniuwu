65212f9b182cdcefbbe9cef20f63a876373068af
Timama <76005360+Timamazzz@users.noreply.github.com>
2022-07-18 18:53:25 +0300

    modify localization

---
+++ lang/en/anime.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'comment' => 'comment',
+    'comments' => 'comments',
+];
--- lang/en/app.php
+++ lang/en/app.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'login' => 'Login'
+    'login' => 'Login',
+    'send' => 'Send'
 ];
